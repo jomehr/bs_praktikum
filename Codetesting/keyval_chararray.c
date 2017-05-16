28f937af3102f42863a75468489d9b154577c13f
vseetohu <vimal_darius.seetohul@smail.th-koeln.de>
2017-05-16 13:01:19 +0200

    Keyval minor improvements

--- Codetesting/keyval_chararray.c
+++ Codetesting/keyval_chararray.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-//#include <time.h>
-//#include <stdlib.h>
 #include <string.h>
+
 #define ROW 100
-#define COL 3
+#define COL 2
+
 int x=0;
-int j=0;
-int index_int=1;
 char value[20];
 char key[20];
 
@@ -18,14 +16,8 @@ char* del(char *key, char *keyval[ROW][COL]);
 int main(){
   int itemNum=0;
   char *kv[ROW][COL];
-  int i=0;
-/*
-  for (i=0;i<100;i++) {
-    strcpy(kv[i][0],"IDX");
-    strcpy(kv[i][1],"VLE");
-    //kv[i][1]=genrandnum();
-  }
-*/
+
+
   while(1){
     printf("Select 1:Put 2:Get 3:Delete 4:Exit\n");
     scanf("%i",&itemNum);
@@ -33,25 +25,20 @@ int main(){
       case 1:
         printf("1.Put selected.\n");
         inputKey();
-        //printf("Current value %s. Enter new value:", kv[key][1]);
+        printf("Enter value: ");
         scanf("%s",value);
-
         put(key, value, kv);
-        printf("Put: key %s, value %s\n",kv[0][1], kv[0][2]);
-        //printf("New value is %i at key %i\n",kv[key][1],key);
+        printf("Put: key %s, value %s\n",kv[0][0], kv[0][1]);
         break;
       case 2:
         printf("2.Get selected.\n");
         inputKey();
-        printf("%s\n", get(key, kv));
-        //printf("Zeile %i : %i, %i\n",key+1, kv[key][0], kv[key][1]);
+        printf("Value: %s\n", get(key, kv));
         break;
       case 3:
         printf("3.Delete selected.\n");
         inputKey();
-        strcpy(value,"haha");//test
-        //strcpy(value, del(*key, kv));
-        printf("The value at key %s is now %s\n", key, value);
+        del(key,kv);
         break;
       default:
         printf("Program ended.\n");
@@ -67,34 +54,46 @@ int inputKey(){
 }
 
 char* get(char *key, char *keyval[ROW][COL]){
-  //strcpy(value,"-1");
-  for(x=0;x<ROW;x++){
-    if(strcmp(keyval[j][1],key)==0){
-      //strcpy(value,keyval[j][1]);
-      //return *value;
-      return keyval[j][2];
+  strcpy(value,"-1");
+  for (x=0; x <ROW;x++) {
+    if(strcmp(keyval[x][0],key)==0){
+      return keyval[x][1];
     }
   }
   return value;
 }
 
 int put(char *key, char *value, char *keyval[ROW][COL]){
-  keyval[j][1]=key;
-  keyval[j][2]=value;
-  //strcpy(keyval[j][1],&value);
-  index_int++;
-  return 1;//success
+  int i;
+  for (x=0;x<ROW;x++) {
+    if(strcmp(keyval[x][0],key)==0){
+      strcpy(keyval[x][1],value);
+      return 1;
+    }
+  }
+  for(i=0;i<ROW;i++) {
+    if(keyval[x][0]==NULL){
+      strcpy(keyval[x][0],key);
+      strcpy(keyval[x][1],value);
+      return 1;
+    }
+  }
+  return -1;
 }
 
 char* del(char *key, char *keyval[ROW][COL]){
   strcpy(value,"-1");
-  for(x=0;x<ROW;x++){
-    if(strcmp(keyval[x][1],key)==0){
-      keyval[x][1]=0;
-      strcpy(value,"0");
-      index_int--;
-      return value;
+  char* tmp;
+  strcpy(value,tmp);
+  for (x=0;x<ROW;x++) {
+    if(strcmp(keyval[x][0],key)==0){
+      *keyval[x][0]=NULL;
+      *keyval[x][1]=NULL;
+      break;
     }
+    return tmp;
+
   }
+
   return value;
 }
