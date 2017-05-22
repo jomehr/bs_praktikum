83f1a626bac1ab351a843e2fa93b29056671edea
Jan Mehr <janmehr93@gmx.de>
2017-05-22 21:03:16 +0200

    test für kommunikation mit telnet

--- Codetesting/server2.c
+++ Codetesting/server2.c
@@ -1,6 +1,5 @@
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -8,51 +7,156 @@
 #include <stdio.h>
 #include <string.h>
 #define BUF 1024
+#define ROW 100
+#define COL 3
+char value[20];
+char key[20];
+int x=0;
+int j=0;
+int index_int=1;
+int itemNum=0;
+char *kv[ROW][COL];
+int i=0;
 
-int sock;
-struct sockaddr_in client;      // Socketadresse eines Clients
-in fd;                          // Filedeskriptor für das Socket
-int client_len;                 // Länge der Client-Daten
-char in[2000];                  // Daten vom Client an den Server
-char out[2000];                 // Daten vom Server an den Client
-client_len = sizeof(client);    // Größe der Client-Nachricht
+int inputKey();
+char* get(char *key, char *keyval[ROW][COL]);
+int put(char *key, char *value, char *keyval[ROW][COL]);
+char* del(char *key, char *keyval[ROW][COL]);
 
-sock = socket(AF_INET, SOCK_STREAM, 0);
+int main (void) {
+	int create_socket, new_socket;
+	socklen_t addrlen;
+	char *buffer = malloc (BUF);
+	ssize_t size;
+	struct sockaddr_in address;
+	const int y = 1;
+	char res[20];
 
-if (sock < 0){
-	perror("creating stream socket");
-	exit(2);
+	if ((create_socket=socket (AF_INET, SOCK_STREAM, 0)) > 0)
+		printf ("Socket wurde angelegt\n");
+
+	setsockopt( create_socket, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(int));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons (1500);
+
+	if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0) {
+		printf( "Der Port ist nicht frei – belegt!\n");
+	}
+
+	listen (create_socket, 5);
+	addrlen = sizeof (struct sockaddr_in);
+
+	while (1) {
+		new_socket = accept ( create_socket, (struct sockaddr *) &address, &addrlen );
+
+		if (new_socket > 0)
+			printf ("Ein Client (%s) ist verbunden ...\n", inet_ntoa (address.sin_addr));
+
+		do {
+			char string[50];
+			char* token;
+			size = recv (new_socket, buffer, BUF-1, 0);
+			printf("%s\n", buffer);
+
+			char delimiter[] = " ";
+			token = strtok(string, delimiter);
+
+			while(token != NULL) {
+				printf("Abschnitt gefunden: %s\n", token);
+				// naechsten Abschnitt erstellen
+ 				token = strtok(NULL, delimiter);
+			}
+
+			if( size > 0)
+				buffer[size] = '\0';
+
+		while (strstr (buffer, "quit") != 0);
+
+		close (new_socket);
+	}
+
+	close (create_socket);
+
+	return EXIT_SUCCESS;
+}
 }
 
-struct sockaddr_in server;
-server.sin_familiy = AF_INET;
-server.sin_addr.s_addr = INADDR_ANY;
-server.sin_port = htons(4711);
+int inputKey(){
+    printf("Enter key: ");
+    scanf("%s",key);
+    return 0;
+	}
 
-bind(sock, (struct sockaddr *) &server, sizeof(server));
+char* get(char* key, char* keyval[ROW][COL], char* res){
+  strcpy(res,"-1");
+  int i;
+  for (i=0;i<ROW;i++) {
+    if(strcmp(keyval[i][0],key)==0){
+      strcpy(res,keyval[i][1]);
+      return res;
+		}
+  }
+  return res;
+}
 
-listen(sock, 5);
-
-struct sockaddr_in client;
-int fileDescriptor, client_len;
-client_len = sizeof(client);
-
-fileDescriptor = accept(sock, &client, &client_len);
-
-ssize_t read (FileDescriptor, Buffer, NBytes);
-ssize_t write (FileDescriptor, Buffer, NBytes);
-
-int recv (Socket, Buffer, Length, Flags);
-int send (Socket, Message, Length, Flags);
-
-int main (void);
-{
-while (TRUE) {                                  // Server wartet auf Anfrage
-    fd = accept(sock, &client, &client_len);
-    while (read(fd, in, 2000) > 0)              // Daten vom Socket ==> in
-                                                // Hier wird nun mit den Daten des Clients gearbeitet.
-        write(fd, out, 2000);                   // Daten vom Array out ==> Socket
+int put(char* key, char* value, char* keyval[ROW][COL]){
+  int i;
+  /*for (i=0;i<ROW;i++) {
+    if(strcmp(keyval[i][0],key)==0){
+      strcpy(keyval[i][1],value);
+      return 0;
     }
-    close(fd);                                  // Der Client hat keine Daten mehr zu übertragen.
+  }*/
+  for(i=0;i<ROW;i++) {
+    if(keyval[i][0]==NULL){
+      printf("%s found at %i\n",keyval[i][0],i);
+      strcpy(keyval[i][0],key);
+      printf("Yes1\n");
+      strcpy(keyval[i][1],value);
+      return 0;
+    }
+  }
+  return -1;
 }
 
+char* del(char* key, char* keyval[ROW][COL], char* res){
+  strcpy(res,"-1");
+  int i;
+  for (i=0;i<ROW;i++) {
+    if(strcmp(keyval[i][0],key)==0){
+      strcpy(res,keyval[i][0]);
+      keyval[i][0]=NULL;
+      keyval[i][1]=NULL;
+      break;
+    }
+  }
+  return res;
+}
+
+// while(1){
+// 	printf("Select 1:Put 2:Get 3:Delete 4:Exit\n");
+// 	scanf("%i",&itemNum);
+// 	switch(itemNum){
+// 		case 1:
+// 			printf("1.Put selected.\n");
+// 			inputKey(key);
+// 			printf("Enter value: ");
+// 			scanf("%s",value);
+// 			put(key, value, kv);
+// 			break;
+// 		case 2:
+// 			printf("2.Get selected.\n");
+// 			inputKey(key);
+// 			printf("Value: %s\n", get(key, kv, res));
+// 			break;
+// 		case 3:
+// 			printf("3.Delete selected.\n");
+// 			inputKey(key);
+// 			del(key, kv, res);
+// 			break;
+// 		default:
+// 			printf("Program ended.");
+// 			return 0;
+// 		}
+// 	}
