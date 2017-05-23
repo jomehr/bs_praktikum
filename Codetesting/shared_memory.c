c52185119db1f34a5978966fe628010b93b2d6ef
Christopher Huntscha <christopher-robin.huntscha@smail.th-koeln.de>
2017-05-23 13:47:51 +0200

    zum Mergen des Shared Memories ohne Race Conditions

---
+++ Codetesting/shared_memory.c
@@ -0,0 +1,33 @@
+#define MAXCOUNT 1000000
+#define NUM_OF_CHILDS 2
+#define SEGSIZE sizeof(int)
+
+void main(){
+  id = shmget(IPC_PRIVATE, SEGSIZE, IPC_CREAT|0600);
+  shar_mem= (int*)shmat(id, 0, 0);
+  *shar_mem= 0;
+
+  for(i = 0; i < NUM_OF_CHILDS; i++){
+    pid[i] = fork();
+    if(pid[i] == -1){
+      printf("Kindprozess konnte nicht erzeugt werden!\n");
+      exit(1);
+    }
+  }
+  for(i = 0; i < NUM_OF_CHILDS; i++){
+    if(pid[i] == 0){ /* Kindprozess-spezifischer Code */
+      while(*shar_mem < MAXCOUNT){
+        *shar_mem+= 1;;
+        printf("Dies ist ein Text.")
+      }
+      exit(0);
+    }
+  }
+  /* Der Vaterprozess wartet, bis alle Kindprozessefertig sind.  */
+  for(i = 0; i < NUM_OF_CHILDS; i++){
+    waitpid(pid[i], NULL, 0);
+  }
+  /* Das SharedMemory Segment wird abgekoppelt und freigegeben. */
+  shmdt(shar_mem);
+  shmctl(id, IPC_RMID, 0);
+}
