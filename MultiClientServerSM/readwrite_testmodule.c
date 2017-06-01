0ed89d6118c93a9706f7e9eff204cfcb649074c9
vseetohu <vimal_darius.seetohul@smail.th-koeln.de>
2017-06-01 13:30:09 +0200

    Created working base

---
+++ MultiClientServerSM/readwrite_testmodule.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUF 1024
+#define KV_STRING 1024
+#define RES 1024
+
+struct Data{
+	char key[BUF][KV_STRING];
+	char value[BUF][KV_STRING];
+	int delFlag[BUF];
+	int size;
+	int realSize;
+};
+
+struct Data *KVStore;
+
+int main()
+{
+    char key[BUF];
+	char value[BUF];
+	Data->size=0;
+	Data->realSize=0;
+
+	char readingRow[500];
+
+    FILE *fp;
+
+    int i;
+	
+    /*
+		Opens data savedKVStore.txt for reading
+	*/
+    fp = fopen("savedKVStore.csv","r");
+    if(fp == NULL){
+            printf("Dataerror\n");
+            return 1;
+    }
+	
+	fscanf(fp,"%s",readingRow);
+    Data.size = atoi(strtok(readingRow,";"));
+    Data.realSize = atoi(strtok(readingRow,";"));
+    /*
+		[WIP] Has to jump over size;realSize to delflag;key;value
+	*/
+	for(i=0;i<BUF;i++){
+		for(k=0;k<KV_STRING;k++){
+			Data.delFlag[i][k] = atoi(strtok(readingRow,";"));
+			strcpy(KVStore.key[i][k], strtok(NULL,";"));
+			strcpy(KVStore.value[i][k], strtok(NULL,";"));
+		}
+	}
+    fclose(fp);
+
+    /*
+		Our while loop
+    */
+
+    /*
+		Opens data savedKVStore.csv for writing
+		Alternative: .txt
+	*/
+    fp = fopen("savedKVStore.csv","w");
+    if(fp == NULL){
+            printf("Dataerror\n");
+            return 1;
+    }
+
+    /*  Writes in order:
+        size;realSize
+		flag1;key1;value1
+		delflag2;key2;value2
+    */
+	fprintf(fp , "%i;%i\n" , Data.size,Data.realSize);
+    for(i=0;i<BUF;i++){
+		for(k=0;k<KV_STRING;k++){
+            fprintf(fp , "%i;%s;%s\n" , Data.delFlag[i][k],Data.key,Data.value[i][k]);
+		}
+	}
+    fclose(fp);
+	
+    return 0;
+}
---
+++ MultiClientServerSM/server.c
@@ -0,0 +1,184 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include "kvs.c"
+
+#define BUF 1024
+#define RES 1024
+
+/*sharedmemory-spezifisch
+#define NUM_OF_CHILDS 5
+#define PROCESSMEM 1024*/
+
+int main (void) {
+	int create_socket, new_socket;
+	socklen_t addrlen;
+	char* buffer = malloc (BUF);
+	ssize_t size;
+	struct sockaddr_in address;
+	const int y = 1;
+	
+	char* token[100];
+	char* separator = " ";
+	char key[BUF];
+	char value[BUF];
+	char res[BUF];
+	
+	struct Data *shmdata;
+	int shmid, pid;
+
+	/*sharedmemory-spezifisch
+    int i, shmid,semid, *shar_mem;
+    int pid[NUM_OF_CHILDS];
+	static int DeleteShmid = 0;
+	void *shmdata;
+
+	SharedMemory erstellen
+    shmid = shmget (IPC_PRIVATE, sizeof(struct KVStore), IPC_CREAT | 0666);
+	if (shmid == -1)
+		printf ("Fehler bei key %d, mit der Größe %d\n",
+        IPC_PRIVATE, PROCESSMEM);
+		DeleteShmid = shmid;
+
+	Shared-Memory-Segment anbinden 
+	shmdata = (struct KVStore *)shmat (shmid, 0, 0);
+	if (shmdata == (void *) -1)
+		printf ("Fehler bei shmat(): shmid %d\n", shmid);
+
+	Kennung am Anfang des Segments schreiben
+	*(int *) shmdata = semid;
+	buffer = shmdata + sizeof (int);
+	printf ("Server läuft mit Shared-Memory-ID %d\n", shmid);
+
+	Das SharedMemory Segment wird abgekoppelt und freigegeben.
+    shmdt(shar_mem);
+    shmctl(shmid, IPC_RMID, 0);*/
+
+
+	if((create_socket=socket (AF_INET, SOCK_STREAM, 0)) > 0){
+		printf ("Socket created!\n");
+	}
+
+	setsockopt( create_socket, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(int));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons (1500);
+
+	if(bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0){
+		printf( "The port is not free – busy!\n");
+	}
+
+	if(listen(create_socket, 5)<0){
+		printf("Error on listening!\n");
+	}
+	
+	/*0644 to create a new segment with rw-r--r rights
+	0777 to create a new segment with unlimited access for all users*/
+	shmid = shmget (IPC_PRIVATE, sizeof(struct Data), IPC_CREAT|0777);
+	/*if ((shmid = shmget (IPC_PRIVATE, sizeof(struct Data), IPC_CREAT|0777)) == -1){
+		printf ("Fehler bei key %d, mit der Größe %d\n",
+        IPC_PRIVATE, sizeof(struct Data));
+		//DeleteShmid = shmid;
+	}*/
+	/*attaches a shared memory segment identified by the variable shmid to the address space of the calling process*/
+	shmdata = (struct Data *) shmat(shmid,0,0);
+	
+	shmdata->size=0;
+	shmdata->realSize=0;
+	
+	addrlen = sizeof (struct sockaddr_in);
+
+	/*Kindprozesse erzeugen
+	for(i = 0; i < NUM_OF_CHILDS; i++) {
+		pid[i] = fork();
+		if (pid[i] == -1) {
+			printf("Kindprozess konnte nicht erzeugt werden!\n");
+			exit(1);
+		}
+	}*/
+
+	while(1){
+		new_socket = accept ( create_socket, (struct sockaddr *) &address, &addrlen );
+		pid=fork();
+		if(pid<0){
+            printf("Fork not successful!\n");
+            exit(1);
+        }else if(pid>0){
+            //parent process
+            close(new_socket);
+        }else if(pid == 0){
+            //child process
+			close(create_socket);
+		
+		printf ("Client (%s) is connected!\n", inet_ntoa (address.sin_addr));
+		char menu[] = "Select put | get | del | list | quit: \n";
+		write(new_socket, menu, strlen(menu));
+
+		do{
+			bzero(buffer,BUF);
+			recv (new_socket, buffer, BUF, 0);
+			//removes trailing line from buffer
+			if ((strlen(buffer)>0) && (buffer[strlen (buffer) - 1] == '\n')){
+				buffer[strlen (buffer) - 1] = '\0';
+			}
+			printf("%s\n", buffer);
+			//adds a trailing whitespace to the buffer
+			if ((buffer[strlen (buffer) - 2] != ' ')){
+				buffer[strlen (buffer) - 1] = ' ';
+			}
+			
+			strtoken(buffer, separator, token, 3);
+			
+			if( strcmp(token[0], "put")==0){
+				printf("Executing put function...\n");
+				put(token[1], token[2], res, shmdata);
+				write(new_socket, res, RES);
+				write(new_socket, "\n", 2);
+			}else if (strcmp(token[0], "get")==0){
+				printf("Executing get function...\n");
+				get(token[1], res, shmdata);
+				write(new_socket, res, RES);
+				write(new_socket, "\n", 2);
+			}else if( strcmp(token[0], "del")==0){
+				printf("Executing del function...\n");
+				del(token[1], res, shmdata);
+				write(new_socket, res, RES);
+				write(new_socket, "\n", 2);
+			}else if( strcmp(token[0], "list")==0){
+				printf("Executing list function...\n");
+				bzero(res, RES);
+				list(res, shmdata);
+				write(new_socket, res, RES);
+			}else{
+				printf("Invalid input!\n");
+				write(new_socket, "Invalid input!\n", 29);
+			}
+			bzero(res, RES);
+		}while(strstr(buffer, "quit") == 0);
+		printf("Executing quit...\n");
+
+		/* Der Vaterprozess wartet, bis alle Kindprozessefertig sind.
+		for(i = 0; i < NUM_OF_CHILDS; i++){
+			waitpid(pid[i], NULL, 0);
+		}
+
+		Das SharedMemory Segment wird abgekoppelt und freigegeben.
+		shmdt(shar_mem);
+		shmctl(shmid, IPC_RMID, 0);*/
+
+		close (new_socket);
+		//close (create_socket);
+		//return EXIT_SUCCESS;
+		}
+	}
+	return 0;
+}
