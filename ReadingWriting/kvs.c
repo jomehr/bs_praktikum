ddf59d12e7e63a6fab9eda50c9615736cbbe0e78
Christopher Huntscha <christopher-robin.huntscha@smail.th-koeln.de>
2017-06-07 18:51:26 +0200

    Differenziertes Schreiben/Lesen ohne Size/Realsize

---
+++ ReadingWriting/kvs.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <string.h>
+#define BUF 1024
+#define KV_STRING 1024
+
+struct Data{
+	char key[BUF][KV_STRING];
+	char value[BUF][KV_STRING];
+	int delFlag[BUF];				//1 = deleted
+	int size;						//last array index
+	int realSize;					//no of entries in array
+};
+char buf[BUFSIZ];
+
+int strtoken(char* str, char* separator, char** token, int size);
+int put(char* key, char* value, char* res, struct Data *shmdata);
+int get(char* key, char* res, struct Data *shmdata);
+int del(char* key, char* res, struct Data *shmdata);
+void list(char* res, struct Data *shmdata);
+
+int strtoken(char* str, char* separator, char** token, int size) {
+	int i=0;
+	token[0] = strtok(str, separator);
+	while(token[i++] && i < size)
+	token[i] = strtok(NULL, separator);
+	return (i);
+}
+
+int put(char* key, char* value, char* res, struct Data *shmdata){
+	int i;
+	for(i=0;i<shmdata->size;i++){
+		if(strcmp(shmdata->key[i],key)==0){
+			strcpy(shmdata->value[i],value);
+			strcpy(res,"Put successful! Existing entry replaced.");
+			return 1;
+		}
+	}
+	for(i=0;i<shmdata->size;i++){
+		if(shmdata->delFlag[i]==1){
+			strcpy(shmdata->key[i],key);
+			strcpy(shmdata->value[i],value);
+			shmdata->delFlag[i]=0;
+			shmdata->realSize++;
+			strcpy(res,"Put successful! Deleted entry replaced.");
+			return 2;
+		}
+	}
+	strcpy(shmdata->key[shmdata->size],key);
+	strcpy(shmdata->value[shmdata->size],value);
+	shmdata->size++;
+	shmdata->realSize++;
+	strcpy(res,"Put successful!");
+	return 0;
+}
+
+int get(char* key, char* res, struct Data *shmdata){
+	strcpy(res, buf);
+	strcat(res,"\n");
+	if(shmdata->size == 0){
+		strcpy(res,"Store is empty.");
+		return 1;
+	}
+	int i;
+	for (i=0; i<shmdata->size;i++) {
+		if(strcmp(shmdata->key[i],key)==0){
+			strcpy(res,shmdata->value[i]);
+			return 0;
+		}
+	}
+	strcpy(res,"Key not found!");
+	return 1;
+}
+
+int del(char* key, char* res, struct Data *shmdata){
+	strcpy(res, "");
+	if(shmdata->size == 0){
+		strcpy(res,"Store is empty.");
+		return 1;
+	}
+	int i;
+	for(i=0;i<shmdata->size;i++){
+		if(strcmp(shmdata->key[i],key)==0){
+			strcpy(res,shmdata->value[i]);
+			shmdata->delFlag[i]=1;
+			strcpy(shmdata->key[i],"");
+			strcpy(shmdata->value[i],"");
+			shmdata->realSize--;
+			return 0;
+		}
+	}
+	strcpy(res,"Key not found!");
+	return 1;
+}
+
+void list(char* res, struct Data *shmdata){
+	strcpy(res, "Size: ");
+	//int converts to char array
+	snprintf(buf, sizeof(buf), "%d", shmdata->size);
+	strcat(res, buf);
+	strcat(res,"\n");
+	if(shmdata->size==0){
+		printf("Store is empty.");
+		return;
+	}
+	int i;
+	for(i=0;i<shmdata->size;i++){
+		if(shmdata->delFlag[i]!=1){
+			printf("Index %i\tKey: %s\tValue: %s\n", i, shmdata->key[i], shmdata->value[i]);
+			strcat(res,shmdata->key[i]);
+			strcat(res," | ");
+			strcat(res,shmdata->value[i]);
+			strcat(res,"\n");
+		}
+	}
+	return;
+}
---
+++ ReadingWriting/serverohnerealsize.c
@@ -0,0 +1,179 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include "kvs.c"
+
+#define BUF 1024
+#define RES 1024
+
+int main (void) {
+	const int y = 1;
+	int create_socket, new_socket, i, k, shmid, pid, mainquit=0;
+	
+	socklen_t addrlen;
+	ssize_t size;
+	
+	struct sockaddr_in address;
+	struct Data *shmdata;
+	
+	char* buffer = malloc (BUF);
+	char* token[100];
+	char* separator = " ";
+	char key[BUF],value[BUF],res[BUF], readingRow[BUF];
+	FILE *fp;
+
+	if((create_socket=socket (AF_INET, SOCK_STREAM, 0)) > 0){
+		printf ("Socket created!\n");
+	}
+
+	setsockopt( create_socket, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(int));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons (1500);
+
+	if(bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0){
+		printf( "The port is not free - busy!\n");
+	}
+
+	if(listen(create_socket, 5)<0){
+		printf("Error on listening!\n");
+	}
+
+	/*0644 to create a new segment with rw-r--r rights
+	0777 to create a new segment with unlimited access for all users*/
+	shmid = shmget (IPC_PRIVATE, sizeof(struct Data), IPC_CREAT|0777);
+
+	/*attaches a shared memory segment identified by the variable shmid to the address space of the calling process*/
+	shmdata = (struct Data *) shmat(shmid,0,0);
+	
+	/*Should be 0 when initialising*/
+	shmdata->size=5;
+	shmdata->realSize=5;
+	
+	/*
+		Reading File Operation has to be placed here after Shared Memory is created
+	*/
+	fp = fopen("reading.csv","r");
+    if(fp == NULL){
+            printf("Keine Datei gefunden\n");
+            return 1;
+    }
+
+	fscanf(fp,"%s",readingRow);
+    for(i=0;i<5;i++){
+        shmdata->delFlag[i]= atoi(strtok(readingRow,";"));
+		strcpy(shmdata->key[i], strtok(NULL,";"));
+        strcpy(shmdata->value[i], strtok(NULL,";"));
+		fscanf(fp,"%s",readingRow);
+	
+    }
+
+    fclose(fp);
+	
+	printf("\nFirst 3 Entries after File Reading Operation\n");
+	printf("Size: %i \t RealSize: %i\n",shmdata->size, shmdata->realSize);
+	for(i=0;i<3;i++){
+		printf("DelFlag: %i \t Key: %s \t Value: %s \n", shmdata->delFlag[i], shmdata->key[i], shmdata->value[i]);
+		// printf("DelFlag: %i \t Key: %s \t Value: %s \n", shmdata->delFlag[1], shmdata->key[1], shmdata->value[1]);
+		// printf("DelFlag: %i \t Key: %s \t Value: %s \n", shmdata->delFlag[2], shmdata->key[2], shmdata->value[2]);
+	}
+	printf("\nList Operation\n");
+	
+	
+	bzero(res, RES);
+	list(res, shmdata);
+
+	
+	addrlen = sizeof (struct sockaddr_in);
+
+	while(mainquit==0){
+		new_socket = accept ( create_socket, (struct sockaddr *) &address, &addrlen );
+		pid=fork();
+		if(pid<0){
+			printf("Fork not successful!\n");
+			exit(1);
+		}else if(pid>0){
+			//parent process
+			close(new_socket);
+		}else if(pid == 0){
+			//child process
+			printf("%i\n",getpid());
+			close(create_socket);
+			printf ("Client (%s) is connected!\n", inet_ntoa (address.sin_addr));
+			char menu[] = "Select: put | get | del | list | quit\n";
+			write(new_socket, menu, strlen(menu));
+
+			do{
+				bzero(buffer,BUF);
+				recv (new_socket, buffer, BUF, 0);
+				//removes trailing line from buffer
+				if ((strlen(buffer)>0) && (buffer[strlen (buffer) - 1] == '\n')){
+					buffer[strlen (buffer) - 1] = '\0';
+				}
+				printf("%s\n", buffer);
+				//adds a trailing whitespace to the buffer
+				if ((buffer[strlen (buffer) - 2] != ' ')){
+					buffer[strlen (buffer) - 1] = ' ';
+				}
+				strtoken(buffer, separator, token, 3);
+
+				if( strcmp(token[0], "put")==0){
+					printf("Executing put function...\n");
+					put(token[1], token[2], res, shmdata);
+					write(new_socket, res, RES);
+					write(new_socket, "\n", 2);
+				}else if (strcmp(token[0], "get")==0){
+					printf("Executing get function...\n");
+					get(token[1], res, shmdata);
+					write(new_socket, res, RES);
+					write(new_socket, "\n", 2);
+				}else if( strcmp(token[0], "del")==0){
+					printf("Executing del function...\n");
+					del(token[1], res, shmdata);
+					write(new_socket, res, RES);
+					write(new_socket, "\n", 2);
+				}else if( strcmp(token[0], "list")==0){
+					printf("Executing list function...\n");
+					bzero(res, RES);
+					list(res, shmdata);
+					write(new_socket, res, RES);
+				}else{
+					printf("Invalid input!\n");
+					char invinp[] =  "Invalid input!\n";
+					write(new_socket, invinp, strlen(invinp));
+				}
+				bzero(res, RES);
+			}while(strstr(buffer, "quit") == 0);
+			printf("Executing Quit\n");
+			close (new_socket);
+			close (create_socket);
+			mainquit=1;
+		}
+		
+	}
+	printf("Exiting Main-While-Loop with Mainquit");
+			
+	/*
+		Writing File Operation
+	*/	
+	fp = fopen("writing.csv","w");
+	if(fp == NULL){
+		printf("No file found!\n");
+	}
+	for(i=0;i<BUF;i++){
+		fprintf(fp, "%i;%s;%s;\n", shmdata->delFlag[i], shmdata->key[i], shmdata->value[i]);
+	}
+
+	fclose(fp);
+			
+	return EXIT_SUCCESS;
+}
