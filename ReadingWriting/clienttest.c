d724bb679cb8c03183f1f37cdb5485991686c115
Jan Mehr <janmehr93@gmx.de>
2017-06-09 20:30:42 +0200

    kleine fixes im client

--- ReadingWriting/clienttest.c
+++ ReadingWriting/clienttest.c
@@ -10,15 +10,15 @@
 #define RES 1024
 
 int main(int argc , char *argv[]) {
-  int create_socket;
+  int clientsocket;
   char *buffer = malloc (BUF);
   char *res = malloc (RES);
   struct sockaddr_in address;
   int size;
 
   //Create socket
-	create_socket = socket(AF_INET , SOCK_STREAM , 0);
-	if (create_socket < 0) {
+	clientsocket = socket(AF_INET , SOCK_STREAM , 0);
+	if (clientsocket < 0) {
 		printf("Could not create socket");
     return 1;
 	}
@@ -28,33 +28,41 @@ int main(int argc , char *argv[]) {
   address.sin_addr.s_addr = inet_addr("127.0.0.1");
   address.sin_port = htons (1500);
 
-  if (connect (create_socket, (struct sockaddr *) &address, sizeof (address)) < 0) {
+  if (connect (clientsocket, (struct sockaddr *) &address, sizeof (address)) < 0) {
     printf("Could not connect to Server\n");
     return 1;
   }
   printf("Connected with Server (%s) \n", inet_ntoa(address.sin_addr));
 
   while(1) {
-    /* TODO
+    /*
     hier input als switch create_socket
     */
     //test write Data
-    printf("write a string: \n");
-    fgets(buffer, BUF, stdin);
-    //Send Data
-    if (send(create_socket, buffer, BUF, 0) < 0) {
-      puts("Send failed");
-      return 1;
-    }
-
-    if (recv(create_socket, res, RES, 0) < 0) {
+    if (recv(clientsocket, res, RES, 0) < 0) {
       puts("Receive failed");
       break;
     }
-    puts("Serverreply: ")
+    puts(res);
+    printf("write a string: \n");
+    fgets(buffer, BUF, stdin);
+    //Send Data
+    if (send(clientsocket, buffer, BUF, 0) < 0) {
+      puts("Send failed");
+      return 1;
+    }else {
+      puts("Send succses");
+      puts(buffer);
+    }
+
+    if (recv(clientsocket, res, RES, 0) < 0) {
+      puts("Receive failed");
+      break;
+    }
+    puts("Serverreply: ");
     puts(res);
   }
 
-  close(create_socket);
+  close(clientsocket);
   return 0;
 }
