041c28abd5d875decd770fc0e33cdb55b0246227
vseetohu <vimal_darius.seetohul@smail.th-koeln.de>
2017-06-11 01:42:56 +0200

    header implementiert. partiell getestet!

---
+++ ReadingWriting/header.h
@@ -0,0 +1,30 @@
+#ifndef KVS_H
+#define KVS_H
+
+#define BUF 1024
+#define KV_STRING 1024
+
+struct Data{
+	char key[BUF][KV_STRING];
+	char value[BUF][KV_STRING];
+	int delFlag[BUF];				
+	int size;						
+	int realSize;
+};
+
+extern int strtoken(char* str, char* separator, char** token, int size);
+
+
+extern int put(char* key, char* value, char* res, struct Data *shmdata);
+
+
+extern int get(char* key, char* res, struct Data *shmdata);
+
+
+extern int del(char* key, char* res, struct Data *shmdata);
+
+
+extern void list(char* res, struct Data *shmdata);
+
+
+#endif
